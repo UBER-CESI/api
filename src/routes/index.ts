ba93e11edfe9f60585ad1303131195c1b6586013
Nathan.TienYou <nathan.tienyou@viacesi.fr>
2022-06-14 11:36:34 +0200

    added custom path to AutoRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,49 +3,51 @@ import { init, models } from "model";
 import mongoose from "mongoose";
 const listen_port = process.env.LISTEN_PORT;
 const router = Router();
-
+const app = express();
 function isHex(num: string): boolean {
   return Boolean(num.match(/^0x[0-9a-f]+$/i));
 }
 
-const autoRouter: { [key: string]: (model: mongoose.Model<any>) => void } = {
-  CREATE: (model: mongoose.Model<any>) => {
-    router.put("/", async (req, res) => {
+const autoRouter: {
+  [key: string]: (model: mongoose.Model<any>, router: Router) => void;
+} = {
+  CREATE: (model: mongoose.Model<any>, _router: Router = router) => {
+    _router.put("/", async (req, res) => {
       const single = new model(req.body);
       await single.save();
       return res.status(201).send(single);
     });
   },
-  GET: (model: mongoose.Model<any>) => {
-    router.get("/:id", async (req, res) => {
+  GET: (model: mongoose.Model<any>, _router: Router = router) => {
+    _router.get("/:id", async (req, res) => {
       const single = await model.findOne({ _id: req.params.id });
       if (!single) return res.sendStatus(404);
       return res.send(single);
     });
   },
-  LIST: (model: mongoose.Model<any>) => {
-    router.get("/", async (req, res) => {
+  LIST: (model: mongoose.Model<any>, _router: Router = router) => {
+    _router.get("/", async (req, res) => {
       const multiple = await model.find();
       if (!multiple) return res.sendStatus(404);
       return res.send(multiple);
     });
   },
-  DELETE: (model: mongoose.Model<any>) => {
-    router.delete("/:id", async (req, res) => {
+  DELETE: (model: mongoose.Model<any>, _router: Router = router) => {
+    _router.delete("/:id", async (req, res) => {
       const single = await model.findOneAndDelete({ _id: req.params.id });
       if (!single) return res.sendStatus(404);
       return res.send(single);
     });
   },
-  EDIT: (model: mongoose.Model<any>) => {
-    router.post("/:id", async (req, res) => {
+  EDIT: (model: mongoose.Model<any>, _router: Router = router) => {
+    _router.post("/:id", async (req, res) => {
       await model.updateOne({ _id: req.params.id }, req.body);
       const single = await model.findOne({ _id: req.params.id });
       return res.send(single);
     });
   },
-  SUSPEND: (model: mongoose.Model<any>) => {
-    router.post("/:id/suspend", async (req, res) => {
+  SUSPEND: (model: mongoose.Model<any>, _router: Router = router) => {
+    _router.post("/:id/suspend", async (req, res) => {
       const single = await model.findOne({ _id: req.params.id });
       if (!single) return res.sendStatus(404);
       single.suspendedAt = new Date();
@@ -55,10 +57,12 @@ const autoRouter: { [key: string]: (model: mongoose.Model<any>) => void } = {
   },
 };
 
-models.forEach(({ model, capabilities }) => {
+models.forEach(({ model, capabilities, path }) => {
+  const router2 = Router();
   capabilities.forEach((cap) => {
-    autoRouter[cap]?.(model);
+    autoRouter[cap]?.(model, router2);
   });
+  app.use(path, router);
 });
 
 router.use("/:id", (req, res, next) => {
@@ -73,10 +77,8 @@ router.use("/:id/*", (req, res, next) => {
   next();
 });
 
-const app = express();
-
 app.use(express.json());
-app.use("/", router);
+
 const server = app.listen(listen_port, () => {
   console.log(`App listening on port ${listen_port}`);
 });
