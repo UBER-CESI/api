1435f8e282be01f19239a97f5017ce37d8486531
Nathan.TienYou <nathan.tienyou@viacesi.fr>
2022-06-15 14:43:11 +0200

    Revert "Merge branch 'customers' into main"
    This reverts commit 5db68c63e6f83f6c6cdb7edf872177be687b8c55, reversing
    changes made to 600cfbf6536924f067581cdbe66079d8bae4e50e.

--- src/model/index.ts
+++ src/model/index.ts
@@ -13,40 +13,11 @@ mongoose.connect(process.env.DB_HOST + "/" + process.env.DB_NAME, {
 });
 export default mongoose;
 
-interface ICustomer {
-  _id?: ObjectId;
-  userId: Number;
-  email: string;
-  nickname: string;
-  firstname: string;
-  lastname: string;
-  phoneNumber: string;
-  suspendedAt?: Date;
-}
-
-const usersSchema = new Schema<ICustomer>({
-  userId: Number,
-  email: String,
-  nickname: String,
-  firstname: String,
-  lastname: String,
-  phoneNumber: String,
-  suspendedAt: Date,
-});
-
-const Customer = model<ICustomer>("Customer", usersSchema);
-
 export const models: {
   model: mongoose.Model<any>;
   capabilities: string[];
   path: string;
-}[] = [
-  {
-    model: Customer,
-    capabilities: ["CREATE", "GET", "LIST", "DELETE", "EDIT", "SUSPEND"],
-    path: "/",
-  },
-];
+}[] = [];
 mongoose.connection.on("error", () => {
   throw new Error("MongoDB Connection Error");
 });
