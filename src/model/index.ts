7177a82c7d92dfe46b192964515f7c6678b067a2
Nathan.TienYou <nathan.tienyou@viacesi.fr>
2022-06-15 14:51:36 +0200

    fix merge

--- src/model/index.ts
+++ src/model/index.ts
@@ -5,7 +5,7 @@ import mongoose, {
   ObjectId,
   Number,
 } from "mongoose";
-import { IMenu, IItem, IItemOption } from "./menu";
+import { IMenu, IItem, IItemOption } from "./menu"
 mongoose.connect(process.env.DB_HOST + "/" + process.env.DB_NAME, {
   user: process.env.DB_USER,
   pass: process.env.DB_PASS,
@@ -13,11 +13,46 @@ mongoose.connect(process.env.DB_HOST + "/" + process.env.DB_NAME, {
 });
 export default mongoose;
 
-export const models: {
-  model: mongoose.Model<any>;
-  capabilities: string[];
-  path: string;
-}[] = [];
+
+export interface IRestaurant {
+  _id?: ObjectId;
+  userId: Number;
+  email: string;
+  name: String;
+  phoneNumber: string;
+}
+
+const itemsSchema = new Schema<IItem>({
+  name: String,
+  description: String,
+  allergens: Array<String>,
+  options: Array<IItemOption>,
+  restaurantId: { type: Schema.Types.ObjectId, ref: "Restaurant" },
+});
+
+const menusSchema = new Schema<IMenu>({
+  name: String,
+  description: String,
+  items: Array<Schema.Types.ObjectId>,
+  price: Number,
+  restaurantId: { type: Schema.Types.ObjectId, ref: "Restaurant" },
+});
+
+const restaurantsSchema = new Schema<IRestaurant>({
+  userId: Number,
+  email: String,
+  name: String,
+  phoneNumber: String,
+});
+
+export const Restaurant = model<IRestaurant>("Restaurant", restaurantsSchema);
+export const Menu = model<IMenu>("Menu", menusSchema);
+export const Item = model<IItem>("Item", itemsSchema);
+
+export const models: { model: mongoose.Model<any>; capabilities: string[], path: string }[] =
+  [{ model: Restaurant, capabilities: ["CREATE", "GET", "LIST", "DELETE", "EDIT", "SUSPEND"], path: "/" },
+  { model: Menu, capabilities: ["CREATE", "GET", "LIST", "DELETE", "EDIT",], path: "/:id/menu/" },
+  { model: Item, capabilities: ["CREATE", "GET", "LIST", "DELETE", "EDIT",], path: "/:id/item/" }];
 mongoose.connection.on("error", () => {
   throw new Error("MongoDB Connection Error");
 });
