0e994e5ef336f2ff8e614e89a7307742b5ae1410
Nathan.TienYou <nathan.tienyou@viacesi.fr>
2022-06-27 17:30:25 +0200

    beginning notification triggers

--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -1,4 +1,5 @@
 import webpush, { PushSubscription } from "web-push";
+import { Customer } from "~~/model";
 if (!process.env.VAPID_PUBLICKEY || !process.env.VAPID_PRIVATEKEY)
     throw new Error("no VAPID keys found, please set env vars");
 webpush.setVapidDetails(
@@ -16,3 +17,8 @@ interface PushMessageBody {
 
 export const sendNotification = (subscription: PushSubscription, body: PushMessageBody) =>
     webpush.sendNotification(subscription, JSON.stringify(body))
+Customer.watch([{ $match: { suspend: { 'type': 6 } } }]).on("change", e => {
+    if (e.operationType !== "update") return
+    if (!e.documentKey) return
+    console.log("suspend" + e.documentKey._id)
+})
