4e07a58afca3581ccaeedbbf924ed16a29c738c7
Nathan.TienYou <nathan.tienyou@viacesi.fr>
2022-06-29 14:34:11 +0200

    remove notification triggers from Orders

--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -21,45 +21,4 @@ export function setup() {
 
     sendNotification = (subscription: PushSubscription, body: PushMessageBody) =>
         webpush.sendNotification(subscription, JSON.stringify(body))
-
-    const options = { fullDocument: "updateLookup" };
-    //[{ $match: { subscriptions: { $exists: true } } }]
-    Customer.watch<{ subscriptions: PushSubscription, unsuspend: boolean, suspend: boolean }>([{ $project: { operationType: "$operationType", subscription: "$fullDocument.subscription", unsuspend: { $in: ["suspendedAt", "$updateDescription.removedFields"] }, suspend: { "$cond": [{ "$ifNull": ["$updateDescription.updatedFields.suspendedAt", null] }, true, false] } } }], options).on("change", (change: any) => {
-        console.log("customerChange")
-        if (change.operationType !== "update") return
-        if (!change.suspend && !change.unsuspend) return
-        if (!change.subscription) return
-        const message: PushMessageBody = { body: "" }
-        if (change.suspend) {
-            message.title = "Your account has been suspended!"
-            message.body = "Please contact an administrator to get more details"
-        }
-        if (change.unsuspend) {
-            message.title = "Your account is no longer suspended!"
-            message.body = "We apologize for the inconvenience"
-        }
-        sendNotification(change.subscription, message)
-    })
-
-    Order.watch<{ subscriptions: PushSubscription, unsuspend: boolean, suspend: boolean }>([{ $project: { operationType: "$operationType", status: "$fullDocument.status", customerId: "$fullDocument.customerId" } }], options).on("change", async (change: any) => {
-        if (change.operationType !== "update") return
-        if (change.status != "preparing" && change.status != "delivering" && change.status != "delivered") return
-        if (!change.customerId) return
-        const message: PushMessageBody = { body: "" }
-        if (change.status == "preparing") {
-            message.title = "Your order is being prepared"
-            message.body = "We will update you shortly after"
-        }
-        if (change.status == "delivering") {
-            message.title = "Your order is being delivered"
-            message.body = "The deliverer should contact you soon!"
-        }
-        if (change.status == "delivered") {
-            message.title = "Your order is delivered"
-            message.body = "lorem ipsum dolor"
-        }
-        const single = await Customer.findOne({ _id: change.customerId })
-        if (!single || !single.subscription) return
-        sendNotification(single.subscription, message)
-    })
 }
