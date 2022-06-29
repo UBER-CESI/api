50e2fa59a39f2c6af6ad409a912a145a470236c5
Nathan.TienYou <nathan.tienyou@viacesi.fr>
2022-06-29 12:01:43 +0200

    fix program crashing if no VAPID key

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -115,7 +115,12 @@ const server = app.listen(listen_port, () => {
 
 export default {
   async spawn() {
-    notifications.setup()
+    try {
+      notifications.setup()
+    } catch (error) {
+      console.error(error)
+    }
+
   },
   stop() {
     server.close();
