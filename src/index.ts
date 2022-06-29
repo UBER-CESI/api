571cf883600a404323e659194651e21d12044ed5
Nathan.TienYou <nathan.tienyou@viacesi.fr>
2022-06-29 16:05:30 +0200

    remove cluster

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,4 @@
-import cluster from "node:cluster";
 
-if (cluster.isPrimary) {
-  import("node:os").then((os) => {
-    const totalCPUs = Number(process.env.CPU_NUMBER) || os.cpus().length;
-    console.log(`Number of CPUs is ${totalCPUs}`);
-    console.log(`Master ${process.pid} is running`);
-
-    // Fork workers.
-    for (let i = 0; i < totalCPUs; i++) {
-      cluster.fork();
-    }
-
-    cluster.on("exit", (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} died`);
-      cluster.fork();
-    });
-  });
-} else {
-  import("./services/notifications").then((notifications) => {
-    notifications.setup();
-  });
-}
+import("./services/notifications").then((notifications) => {
+  notifications.setup();
+});
