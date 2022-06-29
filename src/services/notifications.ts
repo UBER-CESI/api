145bae5d3103da4112492b9a03ca05c1200e6945
Nathan.TienYou <nathan.tienyou@viacesi.fr>
2022-06-29 12:06:08 +0200

    wrap notification inside setup function

--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -1,12 +1,4 @@
 import webpush, { PushSubscription } from "web-push";
-if (!process.env.VAPID_PUBLICKEY || !process.env.VAPID_PRIVATEKEY)
-    throw new Error("no VAPID keys found, please set env vars");
-webpush.setVapidDetails(
-    "mailto:admin@cesi-eats.fr",
-    process.env.VAPID_PUBLICKEY,
-    process.env.VAPID_PRIVATEKEY
-);
-
 interface PushMessageBody {
     title?: string
     body: string
@@ -14,7 +6,18 @@ interface PushMessageBody {
     icon?: string
 }
 
-export const sendNotification = (subscription: PushSubscription, body: PushMessageBody) =>
-    webpush.sendNotification(subscription, JSON.stringify(body))
+export var sendNotification: (subscription: PushSubscription, body: PushMessageBody) =>
+    Promise<webpush.SendResult>
 
-export function setup() { }
+export function setup() {
+    if (!process.env.VAPID_PUBLICKEY || !process.env.VAPID_PRIVATEKEY)
+        throw new Error("no VAPID keys found, please set env vars");
+    webpush.setVapidDetails(
+        "mailto:admin@cesi-eats.fr",
+        process.env.VAPID_PUBLICKEY,
+        process.env.VAPID_PRIVATEKEY
+    );
+
+    sendNotification = (subscription: PushSubscription, body: PushMessageBody) =>
+        webpush.sendNotification(subscription, JSON.stringify(body))
+}
