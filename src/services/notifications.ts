96cd90898e0faff7c39ad6a580f6134d44dca8ed
TienYou-Nathan <nathan.tienyou@viacesi.fr>
2022-06-30 17:10:12 +0200

    added deliverer notifications

---
+++ src/services/notifications.ts
@@ -0,0 +1,23 @@
+import webpush, { PushSubscription } from "web-push";
+interface PushMessageBody {
+    title?: string
+    body: string
+    image?: string
+    icon?: string
+}
+
+export var sendNotification: (subscription: PushSubscription, body: PushMessageBody) =>
+    Promise<webpush.SendResult>
+
+export function setup() {
+    if (!process.env.VAPID_PUBLICKEY || !process.env.VAPID_PRIVATEKEY)
+        throw new Error("no VAPID keys found, please set env vars");
+    webpush.setVapidDetails(
+        "mailto:admin@cesi-eats.fr",
+        process.env.VAPID_PUBLICKEY,
+        process.env.VAPID_PRIVATEKEY
+    );
+
+    sendNotification = (subscription: PushSubscription, body: PushMessageBody) =>
+        webpush.sendNotification(subscription, JSON.stringify(body))
+}
